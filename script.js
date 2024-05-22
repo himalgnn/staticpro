fb7d736b287cb07efef8d39109f6ab9e19f62779
Himal Guragain <96119541+himalgnn@users.noreply.github.com>
2024-05-22 12:34:12 +0545

    Added Gallery
    Gallery made functional using jQuery fancybox

---
+++ script.js
@@ -0,0 +1,12 @@
+var toggleMenu = document.querySelector(".toggle-menu");
+
+toggleMenu.addEventListener("click",function(){
+    if(this.nextElementSibling.classList.contains("expand")){
+        this.nextElementSibling.classList.remove("expand");
+    }else{
+        this.nextElementSibling.classList.add("expand");
+    }
+})
+
+
+console.log(toggleMenu);
