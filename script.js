80267d704da5a4892652767734bb7b1e194e62d7
Himal Guragain <96119541+himalgnn@users.noreply.github.com>
2024-05-24 12:58:30 +0545

    Added Form Validation
    Form Validation using JavaScript. And local jquery is added in contact page

--- script.js
+++ script.js
@@ -35,3 +35,75 @@ accordDt.on("click", function(){
     // jQuery(this).next('dd').slideDown(300).siblings('dd').slideUp(500);
     $(this).next('dd').slideToggle(300);
 });
+
+
+/**
+ * ## JavaScript Form validation scripts
+ * ::JavaScript DOM element selectors  (use document key)
+ * 
+ * ie: document.selectorMethod("DOM_NODE_Identifier");
+ * - SelectorMethods
+ *      -getElementById("idname")
+ *      -getElementsByClassName("classname")
+ *      -getElementsByTagName("tagname")
+ *      -querySelector("CSS Selector")
+ *      -querySelectorAll("CSS_Selector")
+ *      -form_name or form_name.field_name
+ * 
+ * 
+ */
+
+
+let formEle = document.contact_form;
+console.log(formEle);
+//let fName = document.contact_form.full_name;
+
+let fName = formEle.fname,
+    addr = formEle.address,
+    email = formEle.email;
+
+formEle.addEventListener("submit", function(e){
+    if(fName.value==""){
+        // alert("Full Name is required!");
+
+        fName.nextElementSibling.innerText = "Full Name is required!";
+        e.preventDefault(); //Stops page reload on form submit
+
+    }
+    if(addr.value==""){
+        // alert("Address is required!");
+        addr.nextElementSibling.innerText = "Address is required!";
+        e.preventDefault(); //Stops page reload on form submit
+        
+    }
+    if(email.value==""){
+        // alert("E-mail is required!");
+        email.nextElementSibling.innerText = "Email is required!";
+        e.preventDefault(); //Stops page reload on form submit
+    }
+});
+
+/**
+ * JavaScript Events
+ * - Window Event (i.e:onload)
+ * - Form event (i.e: onsubmit);
+ * - Keyboard event (ie.:onkeyup)
+ * - Mouse event (i.e. onclick)
+ */
+
+
+
+email.addEventListener("keyup", function(){
+    if(this.value.indexOf('@') == -1){
+        this.nextElementSibling.innerText = "One '@' symbol is required";
+    }else{
+        this.nextElementSibling.innerText = "";
+    }
+});
+
+
+/**
+ * Study Regular Expression (RegEx)
+ * - regexr.com (Recommended)
+ * - rgex101.com etc.
+ */
